0cad14fb9ab5c1701e8c89a8b7d02524b00359a8
Abdul Yadi <abdulyadi.datatrans@gmail.com>
2020-12-29 19:41:45 +0700

    array unshift

--- beruang/mvp/beruang-array.js
+++ beruang/mvp/beruang-array.js
@@ -6,46 +6,61 @@ class extends base {
   }
 
   push(path, ...items) {//return new array length
-    let objFld = this._getObjField(path);
-    if(!objFld) {
-      return;
-    }
-    let arr = objFld.fld ? objFld.obj[objFld.fld] : objFld.obj;
-    if(arr.constructor.name!='Array'){
+    let obj = this._getArray(path);
+    if(!obj) {
       return 0;
     }
-    let startIdx = arr.length;
+    let startIdx = obj.arr.length;
     items.forEach((item, i) => {
-      arr.push(item);
+      obj.arr.push(item);
     });
-    this.view.arrayPush(path, objFld.prop, startIdx, items.length);
-    return arr.length;
+    this.view.arrayPush(path, obj.prop, startIdx, items.length);
+    return obj.arr.length;
   }
 
   splice(path, index, removeCount, ...items) {//return removed count
-    let objFld = this._getObjField(path);
-    if(!objFld) {
-      return;
-    }
-    let arr = objFld.fld ? objFld.obj[objFld.fld] : objFld.obj;
-    if(arr.constructor.name!='Array'){
+    let obj = this._getArray(path);
+    if(!obj) {
       return 0;
-    }    
-    if(index>=arr.length) {
+    }
+    if(index >= obj.arr.length) {
       this.push(path, items);
       return 0;
     }
-    let len0 = arr.length;
+    let len0 = obj.arr.length;
     for(let i=0; i<removeCount; i++) {
-      arr.splice(index, 1);
+      obj.arr.splice(index, 1);
     }
-    let removes = len0 - arr.length;
+    let removes = len0 - obj.arr.length;
     let pushIdx = index;
     items.forEach((item, i) => {
-      arr.splice(pushIdx++, 0, item);
+      obj.arr.splice(pushIdx++, 0, item);
     });
-    this.view.arraySplice(path, objFld.prop, index, items.length, removeCount);
+    this.view.arraySplice(path, obj.prop, index, items.length, removeCount);
     return removes;
+  }
+
+  unshift(path, ...items) {//return new length
+    let obj = this._getArray(path);
+    if(!obj) {
+      return 0;
+    }
+    let pushIdx = 0;
+    items.forEach((item, i) => {
+      obj.arr.splice(pushIdx++, 0, item);
+    });
+    this.view.arraySplice(path, obj.prop, 0, items.length, 0);
+    return obj.arr.length;
+  }
+
+  _getArray(path) {
+    let objFld = this._getObjField(path);
+    if(!objFld) {
+      return null;
+    }
+    let arr = objFld.fld ? objFld.obj[objFld.fld] : objFld.obj;
+    return arr && arr.constructor.name=='Array' ?
+      {'arr':arr, 'prop':objFld.prop} : null;
   }
 
 }
